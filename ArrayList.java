6c0e6428d63c7892979f1e4b49b5e69645e872ab
Giovanni Rivera <grivera64@toromail.csudh.edu>
2022-09-16 11:34:33 -0700

    [ArrayList impl]: Add ArrayList.java template

---
+++ ArrayList.java
@@ -0,0 +1,39 @@
+package com.grivera64.datastructure.List;
+
+public class ArrayList<E> implements List<E> {
+
+    @Override
+    public void add(E element) {
+
+    }
+
+    @Override
+    public void add(E element, int index) {
+
+    }
+
+    @Override
+    public E remove(int index) {
+        return null;
+    }
+
+    @Override
+    public E get(int index) {
+        return null;
+    }
+
+    @Override
+    public void set(E element, int index) {
+
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public int indexOf(E element) {
+        return 0;
+    }
+}
