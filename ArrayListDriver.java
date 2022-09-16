f98dc1bbe730ac544f111ad13164e48dadebb1fb
Giovanni Rivera <grivera64@toromail.csudh.edu>
2022-09-16 14:35:45 -0700

    [ArrayList Impl]: Add ArrayListDriver.java

---
+++ ArrayListDriver.java
@@ -0,0 +1,20 @@
+package com.grivera64.datastructure.list;
+
+public class ArrayListDriver {
+
+    public static void main(String[] args) {
+//        List<Integer> list = new ArrayList<>();
+        List<Integer> list = new ArrayList<>(1);
+
+        list.add(10);
+        System.out.println(list);
+        list.add(20);
+        System.out.println(list);
+        list.add(30);
+        System.out.println(list);
+        list.add(40);
+        System.out.println(list);
+        list.add(50);
+        System.out.println(list);
+    }
+}
